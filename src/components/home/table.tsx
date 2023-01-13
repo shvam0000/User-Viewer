2ce6a59bcab6baac92895952a5d796c59439d040
shvam0000 <shvam0000@gmail.com>
2023-01-13 15:08:58 +0530

    ✨ add table

--- src/components/home/table.tsx
+++ src/components/home/table.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { useTable } from 'react-table';
 import { Badge, Button } from '../shared';
-import { PlusIcon, DownloadIcon } from '../../utils/icons';
+import { PlusIcon, DownloadIcon, ArrowIcon } from '../../utils/icons';
+import { data, columns } from '../../utils/data/table-data';
 
 const Table = () => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    //@ts-ignore
+  } = useTable({ columns, data });
+
   return (
     <div className="border-2 border-gray px-10 py-6 rounded-lg shadow-lg">
       <div className="flex justify-between items-start">
@@ -41,6 +51,47 @@ const Table = () => {
             </Button>
           </div>
         </div>
+      </div>
+      <hr />
+
+      {/* Table starts here */}
+      <div className="bg-white p-4">
+        <table {...getTableProps()} className="table-auto w-full">
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    {...column.getHeaderProps()}
+                    className="px-4 py-2 items-start text-gray-400">
+                    <div className="flex">
+                      <span>{column.render('Header')}</span>
+                      <span className="text-xl font-bold pl-1">
+                        <ArrowIcon />
+                      </span>
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()} className="px-4 py-2">
+                        {cell.render('Cell')}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
---
+++ src/utils/data/table-data.ts
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    name: 'A',
+    age: 20,
+    status: 'active',
+    role: 'admin',
+    lastLogin: '10 Jan, 2023',
+  },
+  {
+    name: 'A',
+    age: 20,
+    status: 'active',
+    role: 'admin',
+    lastLogin: '10 Jan, 2023',
+  },
+  {
+    name: 'A',
+    age: 20,
+    status: 'active',
+    role: 'admin',
+    lastLogin: '10 Jan, 2023',
+  },
+  {
+    name: 'A',
+    age: 20,
+    status: 'active',
+    role: 'admin',
+    lastLogin: '10 Jan, 2023',
+  },
+];
+
+export const columns = [
+  {
+    Header: 'Name ',
+    accessor: 'name',
+  },
+  {
+    Header: 'Status',
+    accessor: 'status',
+  },
+  {
+    Header: 'Role',
+    accessor: 'role',
+  },
+  {
+    Header: 'Last Login',
+    accessor: 'lastLogin',
+  },
+];
