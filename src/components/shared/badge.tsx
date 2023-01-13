ae481a147f46a168ff130bf14eb6ae5e7d8760b8
shvam0000 <shvam0000@gmail.com>
2023-01-13 13:50:40 +0530

    💄 UI changes

--- src/components/shared/badge.tsx
+++ src/components/shared/badge.tsx
@@ -6,7 +6,7 @@ const Badge: React.FC<BadgeProps> = ({ type, children }) => {
     <div
       className={`text-sm font-medium px-2 py-1 rounded-xl  w-fit ${
         type === 'primary'
-          ? 'bg-green-100 text-green-900'
+          ? 'bg-green-100 text-green-700'
           : 'bg-gray-200 text-gray-800'
       }`}>
       {children}
--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<ButtonProps> = ({ type, children }) => {
       className={`text-md px-4 py-2 rounded-md w-fit ${
         type === 'primary'
           ? 'bg-blue-400 text-gray-100'
-          : 'bg-white border-2 border-gray-400 text-gray-800 font-medium'
+          : 'bg-white border-2 border-gray-200 text-gray-700 font-medium'
       }`}>
       {children}
     </div>
