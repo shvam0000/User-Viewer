eba65b12e83dc50180603eb8ba9520924fb0a5a8
shvam0000 <shvam0000@gmail.com>
2023-01-14 17:22:15 +0530

    ⚡️ find single user

--- api/routes/user.js
+++ api/routes/user.js
@@ -74,4 +74,20 @@ router.delete('/:userId', (req, res) => {
     });
 });
 
+router.get('/:userId', (req, res) => {
+  const id = req.params.userId;
+  User.findById(id)
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
 module.exports = router;
