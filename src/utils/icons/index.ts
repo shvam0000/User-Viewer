d0e411f1f45110b1b6f9ab9f6afbc3d8e9c5a6bc
shvam0000 <shvam0000@gmail.com>
2023-01-14 12:01:09 +0530

    ✨ add edit and delete icons

--- src/utils/icons/index.ts
+++ src/utils/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as DownloadIcon } from './download-icon';
 export { default as PlusIcon } from './plus-icon';
 export { default as ArrowIcon } from './arrow-icon';
+export { default as EditIcon } from './edit-icon';
+export { default as DeleteIcon } from './delete-icon';
